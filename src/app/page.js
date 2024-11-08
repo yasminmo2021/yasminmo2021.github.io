ff3a2ed85281f1ab94610a22f8f62b1bde58c470
yasminmo2021 <159488286+yasminmo2021@users.noreply.github.com>
2024-11-08 14:29:59 -0400

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Meu título</h1>
+      <h1>Volei</h1>
       <Image className={styles.img} src='/images/OIP.jpg' alt='Volei' width={400}  height={400}/>
     </div>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,23 @@
 import Link from "next/link";
 import styles from "./Header.module.css";
 
+
 export default function Header(){
     return(
         <header>
             <nav>
                 <ul className={styles.lista}>
-                    <li>
+                    <li className={styles.home}>
                         <Link href='/'>Home</Link>
                     </li>
                     <li>
                         <Link href="/Sobre">Sobre</Link>
                     </li>
+                    <li>
+                        <Link href="/Quadra">Qudra</Link>
+                    </li>
                 </ul>
+                
             </nav>
         </header>
     )
